aa92fc6e803e7676f87fd2ef0524c72c99a8d907
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-08-24 03:48:36 -0700

    -

---
+++ script.py
@@ -0,0 +1,159 @@
+import json
+layout = {
+	"title":"Conscript Keyboard",
+	"onScreen": {
+		"main":[]
+	}
+}
+def uni(stringInput):
+	uniDict = {
+		"d;":"ð",
+		"e;":"é",
+		"i;":"í",
+		"n;":"ŋ",
+		"o;":"ó",
+		"s;":"ś",
+		"t;":"þ",
+		"u;":"ú",
+		"u;;":"ű",
+		"z;":"ź"
+	}
+	stringInput = stringInput.lower()
+	stringOutput = ""
+	if stringInput in uniDict:
+		stringOutput = uniDict[stringInput]
+	else:
+		stringOutput = stringInput
+	return stringOutput
+def menu(arrayInput):
+	stringOutput = "".join(arrayInput)
+	return f"[XC:{stringOutput}]"
+def swipe(hashInput):
+	arrayOutput = []
+	for i in "C W N E S NE NW SW SE".split():
+		if i not in hashInput:
+			hashInput[i] = " "
+		arrayOutput.append(hashInput[i])
+	stringOutput = "".join(arrayOutput).rstrip()
+	return f"[4D:{stringOutput}]"
+def multi(arrayInput):
+	stringOutput = "".join(arrayInput)
+	return f"[MC:{stringOutput}]"
+def key(stringInput, width = 0):
+	stringOutput = f"{stringInput}{'[]'*(width-1)}"
+	return stringOutput
+def row(arrayInput):
+	stringOutput = "".join(arrayInput)
+	layout["onScreen"]["main"].append(stringOutput)
+	return
+row([
+	key("x"),
+	key("a"),
+	key(swipe({
+		"C":"e",
+		"N":uni("e;")
+	})),
+	key(swipe({
+		"C":"i",
+		"N":uni("i;")
+	})),
+	key(swipe({
+		"C":"o",
+		"N":"o;"
+	})),
+	key(swipe({
+		"C":"u",
+		"N":uni("u;"),
+		"S":uni("u;;")
+	})),
+	key("'")
+])
+row([
+	key(swipe({
+		"C":"c",
+		"N":"j"
+	})),
+	key(swipe({
+		"C":"f",
+		"N":"v"
+	})),
+	key("h"),
+	key(swipe({
+		"C":"k",
+		"N":"g"
+	})),
+	key("l"),
+	key("m"),
+	key("n")
+])
+row([
+	key(uni("n;")),
+	key(swipe({
+		"C":"p",
+		"N":"b"
+	})),
+	key("r"),
+	key(swipe({
+		"C":"s",
+		"N":"z"
+	})),
+	key(swipe({
+		"C":uni("s;"),
+		"N":uni("z;")
+	})),
+	key(swipe({
+		"C":"t",
+		"N":"d"
+	})),
+	key(swipe({
+		"C":uni("t;"),
+		"N":uni("d;")
+	}))
+])
+row([
+	key("[SHIFT]",3),
+	key("w"),
+	key("y"),
+	key("[DEL]",2)
+])
+row([
+	key(
+		swipe({
+			"N":"[UP]",
+			"S":"[DOWN]",
+			"W":"[LEFT]",
+			"E":"[RIGHT]"
+		}),
+		2
+	),
+	key(
+		swipe({
+			"C":".",
+			"N":multi(". . .".split())
+		}),
+		2
+	),
+	key("-"),
+	key(","),
+	key("?")
+])
+row([
+	key(
+		swipe({
+			"C":"[ALL]",
+			"N":"[COPY]",
+			"S":"[PASTE]",
+			"E":"[UNDO]",
+			"W":"[CUT]"
+		}),
+		2
+	),
+	key("[SPACE]",3),
+	key("[ENTER]",2)
+])
+data = json.dumps(layout,ensure_ascii=True)
+with open("test.json","w",encoding="utf-8") as f:
+	f.write(data)
+print()
+print("\n".join(layout["onScreen"]["main"]))
+print()
