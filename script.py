32db52115dc376ceb51d3be9649341026122abd8
JactusTheCactus <threateninglyamusing@gmail.com>
2025-08-24 16:49:52 -0700

    Update script.py

--- script.py
+++ script.py
@@ -46,15 +46,7 @@ def row(arrayInput):
 	stringOutput = "".join(arrayInput)
 	layout["onScreen"]["main"].append(stringOutput)
 	return
-row([
-	key("x"),
-	key("a"),
-	key(swipe({"C":"e","N":uni("e;")})),
-	key(swipe({"C":"i","N":uni("i;")})),
-	key(swipe({"C":"o","N":uni("o;")})),
-	key(swipe({"C":"u","N":uni("u;"),"S":uni("u;;")})),
-	key("'")
-])
+row([key("x"),key("a"),key(swipe({"C":"e","N":uni("e;")})),key(swipe({"C":"i","N":uni("i;")})),key(swipe({"C":"o","N":uni("o;")})),key(swipe({"C":"u","N":uni("u;"),"S":uni("u;;")})),key("'")])
 row([key(swipe({"C":"c","N":"j"})),key(swipe({"C":"f","N":"v"})),key("h"),key(swipe({"C":"k","N":"g"})),key("l"),key("m"),key("n")])
 row([key(uni("n;")),key(swipe({"C":"p","N":"b"})),key("r"),key(swipe({"C":"s","N":"z"})),key(swipe({"C":uni("s;"),"N":uni("z;")})),key(swipe({"C":"t","N":"d"})),key(swipe({"C":uni("t;"),"N":uni("d;")}))])
 row([key("[SHIFT]",3),key("w"),key("y"),key("[DEL]",2)])
