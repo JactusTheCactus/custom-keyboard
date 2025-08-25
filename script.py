c1acdf3bc1808298d0553468e32778e7ab8f5524
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-08-24 23:38:26 -0700

    -

--- script.py
+++ script.py
@@ -69,7 +69,7 @@ def swipe(arrayInput):
 	length = len(newArray)
 	match length:
 		case 1:
-			stringOutput = menu([newArray[0]])
+			stringOutput = newArray[0]
 		case 2:
 			stringOutput = swipeVerbose({
 				"C": newArray[0],
