d1d4afac1cd648c4673e8be0373bae700aed623b
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-08-24 22:42:28 -0700

    -

--- script.py
+++ script.py
@@ -45,24 +45,28 @@ def menu(arrayInput):
 	return f"[XC:{stringOutput}]"
 def swipeVerbose(hashInput):
 	for k, v in hashInput.items():
-		if all([
-			type(hashInput[k]) == str,
-			len(v) > 1,
-			not any([
-				bool(re.search(r"\[\w+\]",char(v))),
-				bool(re.search(r"[^\u0000-\u007f]",char(v)))
-			])
-		]):
-			hashInput[k] = multi(hashInput[k])
+		print(v)
+		if type(v) != int:
+			if all([
+				type(hashInput[k]) == str,
+				len(v) > 1,
+				not any([
+					bool(re.search(r"\[\w+\]",char(v))),
+					bool(re.search(r"[^\u0000-\u007f]",char(v)))
+				])
+			]):
+				hashInput[k] = multi(hashInput[k])
 	arrayOutput = []
 	for i in "C W N E S NW NE SE SW".split():
 		if i not in hashInput:
 			hashInput[i] = " "
 		arrayOutput.append(char(hashInput[i]))
-	stringOutput = "".join(arrayOutput).rstrip()
+	stringOutput = "".join(filter(bool,list(map(lambda i: i if type(i) != int else "",arrayOutput)))).rstrip()
 	return Key(f"[4D:{stringOutput}]")
-def swipe(*arrayInput):
-	match len(arrayInput):
+def swipe(arrayInput):
+	newArray = arrayInput[:-1] if type(arrayInput[-1]) == int else arrayInput
+	length = len(newArray)
+	match length:
 		case 1:
 			return swipeVerbose({
 				"C": arrayInput[0]
@@ -87,65 +91,64 @@ def swipe(*arrayInput):
 				"W": arrayInput[4]
 			}).value
 		case _:
-			print(len(arrayInput))
-			return f"<{len(arrayInput)}>"
-	return
+			return f"<{newArray}>"
 def multi(stringInput):
 	return f"[MC:{char(stringInput)}]"
-def key(stringInput, width = 1):
-	stringOutput = f"{char(stringInput)}{'[]' * (width - 1)}"
+def key(stringInput):
+	width = stringInput[-1] if type(stringInput[:-1]) == int else 1
+	stringOutput = f"{swipe(list(map(lambda i: char(i),stringInput)))}{'[]' * (width - 1)}"
 	key = swipe(stringOutput) if type(stringOutput) == Key else stringOutput
 	return Key(key,width)
 def row(*arrayInput):
-	map(lambda i: key(i),arrayInput)
+	map(lambda i: key([i]),arrayInput)
 	print(sum([i.length for i in arrayInput]))
 	stringOutput = "".join(map(lambda i: i.value,arrayInput))
 	layout["onScreen"]["main"].append(stringOutput)
 	return
 row(
-	key(swipe("x"),2),
-	key("a"),
-	key(swipe("e","e;")),
-	key(swipe("i","i;")),
-	key(swipe("o","o;")),
-	key(swipe("u","u;","u;;")),
-	key(swipe("'"),3)
+	key(["x",3]),
+	key(["a"]),
+	key(["e","e;"]),
+	key(["i","i;"]),
+	key(["o","o;"]),
+	key(["u","u;","u;;"]),
+	key(["'",3])
 )
 row(
-	key(swipe("c","j")),
-	key(swipe("f","v")),
-	key("h"),
-	key(swipe("k","g")),
-	key("l"),
-	key("m"),
-	key("n")
+	key(["c","j"]),
+	key(["f","v"]),
+	key(["h"]),
+	key(["k","g"]),
+	key(["l"]),
+	key(["m"]),
+	key(["n"])
 )
 row(
-	key("n;"),
-	key(swipe("p","b")),
-	key("r"),
-	key(swipe("s","z")),
-	key(swipe("s;","z;")),
-	key(swipe("t","d")),
-	key(swipe("t;","d;"))
+	key(["n;"]),
+	key(["p","b"]),
+	key(["r"]),
+	key(["s","z"]),
+	key(["s;","z;"]),
+	key(["t","d"]),
+	key(["t;","d;"])
 )
 row(
-	key("shift",2),
-	key("w"),
-	key("y"),
-	key(swipe(".","...")),
-	key("del",2)
+	key(["shift",2]),
+	key(["w"]),
+	key(["y"]),
+	key([".","..."]),
+	key(["del",2])
 )
 row(
-	key(swipe("tab","up","down","right","left"),2),
-	key(",",2),
-	key("?",2),
-	key("-")
+	key(["tab","up","down","right","left",2]),
+	key([",",2]),
+	key(["?",2]),
+	key(["-"])
 )
 row(
-	key(swipe("_A","_C","_X","_V","_Z"),2),
-	key("space",3),
-	key("enter",2)
+	key(["_A","_C","_X","_V","_Z",2]),
+	key(["space",3]),
+	key(["enter",2])
 )
 with open("layout.json","w") as f:
 	print("\n".join(layout["onScreen"]["main"]))
