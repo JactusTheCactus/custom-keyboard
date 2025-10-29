ae28a92fac0f3d5e5556716d01d6c6110108e089
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-10-28 20:18:47 -0700

    -

--- .sh
+++ .sh
@@ -5,42 +5,38 @@ flag() {
 		[[ -e ".flags/$f" ]] || return 1
 	done
 }
-if ! flag cloudflare; then
-	pyVer="3.12"
-	alias python="python$pyVer"
-	alias pip="pip$pyVer"
-	rm -rf layouts
-	mkdir -p layouts
-	if ! [[ -d "vEnv" ]]; then
-		python -m venv vEnv
-	fi
-	source vEnv/bin/activate
-	dependencies=(
-		pyyaml
-	)
-	for i in "${dependencies[@]}"; do
-		pip install $i
-	done
-	build() {
-		for in in data/*.yml; do
-			out=$in
-			out=${out%.yml}.json
-			out=layouts${out#data}
-			python script.py $in $out
-		done
-	}
-	WATCH=(
-		*.yml
-		*.json
-		script.py
-	)
-	build
+pyVer="3.12"
+alias python="python$pyVer"
+alias pip="pip$pyVer"
+rm -rf layouts
+mkdir -p layouts
+if ! [[ -d "vEnv" ]]; then
+	python -m venv vEnv
 fi
+source vEnv/bin/activate
+dependencies=(
+	pyyaml
+)
+for i in "${dependencies[@]}"; do
+	pip install $i
+done
+build() {
+	for in in data/*.yml; do
+		out=$in
+		out=${out%.yml}.json
+		out=layouts${out#data}
+		python script.py $in $out
+	done
+}
+WATCH=(
+	*.yml
+	*.json
+	script.py
+)
+build
 ./index.sh
-if ! flag cloudflare; then
-	if flag local; then
-		while inotifywait -e close_write "${WATCH[@]}"; do
-			build
-		done
-	fi
+if flag local; then
+	while inotifywait -e close_write "${WATCH[@]}"; do
+		build
+	done
 fi
