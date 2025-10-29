40ba3f543ef35e8cda50c56e51f612dbefd18582
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-10-28 23:27:58 -0700

    -

--- index.sh
+++ index.sh
@@ -10,17 +10,28 @@ for FILE in layouts/*.json; do
 	mapfile -t ROWS < <(jq -r .onScreen.main[] $FILE)
 	for ROW in "${ROWS[@]}"; do
 		if [ "$FILE" = "layouts/qwerty.json" ]; then
-			ROW="$(echo "$ROW" | perl -CS -pe 's/(\x{0301}|\x{0308}|\x{030c}|\x{0323})/\x{25cc}$1/g')"
+			ROW="$(echo "$ROW" | perl -CS -pe 's/(\x{0301}|\x{0308}|\x{030c})/\x{25cc}$1/g')"
 		fi
-		ROW="$(echo "$ROW" | perl -CS -pe 's/>/&gt;/g')"
-		ROW="$(echo "$ROW" | perl -CS -pe 's/</&lt;/g')"
-		ROW="$(echo "$ROW" | perl -CS -pe 's/\{/&lbrace;/g')"
-		ROW="$(echo "$ROW" | perl -CS -pe 's/\}/&rbrace;/g')"
+		ROW="$(echo "$ROW" | perl -CS -pe 's/(\x{0323})/\x{25cc}$1/g')"
+		RE=(
+			"s/>/&gt;/g"
+			"s/</&lt;/g"
+			"s/\{/&lbrace;/g"
+			"s/\}/&rbrace;/g"
+		)
+		for re in "${RE[@]}"; do
+			ROW="$(echo "$ROW" | perl -CS -pe $re)"
+		done
 		HTML+="<li>$ROW</li>"
 	done
 	HTML=${HTML%"<br>"}
 	HTML+="</ul>"
 done
-HTML="$(echo "$HTML" | perl -CS -pe 's/\x{00C6}/&AElig;/g')"
-HTML="$(echo "$HTML" | perl -CS -pe 's/\x{00E6}/&aelig;/g')"
+RE=(
+	"s/\x{00C6}/&AElig;/g"
+	"s/\x{00E6}/&aelig;/g"
+)
+for re in "${RE[@]}"; do
+	HTML="$(echo "$HTML" | perl -CS -pe $re)"
+done
 echo "$HTML" > index.html
