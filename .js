a8346914c1876e2fd35b5888a787d493e307b837
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-10-31 00:39:26 -0700

    -

--- .js
+++ .js
@@ -7,13 +7,6 @@ const args = process.argv.filter(i => !/node|\.js$/.test(i))
 const inputFile = ["WIP", "data", [args[0], "yml"].join(".")].join("/")
 const outputFile = ["WIP", "layouts", [args[0], "json"].join(".")].join("/")
 const input = YAML.load(fs.readFileSync(inputFile, { encoding: "utf-8" }))
-const out = {
-	title: input.title,
-	onScreen: {
-		main: []
-	}
-}
-const test = "x a e i o u dotBelow".split(/\s+/)
 function menu(arrIn) {
 	return `[XK:${arrIn.map(char).join("")}]`
 }
@@ -23,7 +16,10 @@ function multi(strIn) {
 function swipe(objIn) {
 	return `[4D:${"C W N E S NW NE SE SW"
 		.split(/\s+/)
-		.map(k => char(objIn[k] ?? " "))
+		.map(k => char(objIn[k.toUpperCase()])
+			?? char(objIn[k.toLowerCase()])
+			?? " "
+		)
 		.join("")
 		.replace(/\s*$/, "")
 		}]`
@@ -31,7 +27,14 @@ function swipe(objIn) {
 function char(charIn) {
 	switch (typeof charIn) {
 		case "string":
-			if (charIn.length > 1) {
+			if (
+				charIn.length > 1
+				|| [
+					...Object.keys(uni),
+					...Object.keys(diacritics)
+				]
+					.includes(charIn)
+			) {
 				const arrOut = charIn.split("_")
 				if (arrOut.length > 1) {
 					return arrOut.map(char).join("")
@@ -51,8 +54,26 @@ function char(charIn) {
 			}
 	}
 }
-// function key() { }
-// function row() { }
-// function keyboard() { }
-// fs.writeFileSync(outputFile,JSON.stringify(out,null,"\t"))
-console.log(char(test))
+function key(keyIn) {
+	return keyIn
+		.map(char)
+		.slice(1)
+		.join("")
+		+ "[]".repeat(keyIn[0] - 1)
+}
+function row(rowIn) {
+	return rowIn
+		.map(key)
+		.join("")
+}
+function keyboard(boardIn) {
+	return boardIn.map(row)
+}
+const out = {
+	title: input.title,
+	onScreen: {
+		main: keyboard(input.layout)
+	}
+}
+fs.writeFileSync(outputFile,JSON.stringify(out,null,"\t"))
+console.log(out)
