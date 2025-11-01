5359cae87c2da0c7b2e59d354e137ed93ebddee6
JactusTheCactus <threateninglyamusing@gmail.com>
2025-11-01 02:50:15 -0700

    Update _.ts

--- page/_.ts
+++ page/_.ts
@@ -2,15 +2,22 @@
 	function FMT(strIn: string) {
 		return (strIn || "")
 			.replace(/\[(.*?)\]/g, (_, m) => {
-				switch (m.toLowerCase()) {
-					case "undo": return "CTRL_Z"
-					case "cut": return "CTRL_X"
-					case "copy": return "CTRL_C"
-					case "paste": return "CTRL_V"
-					case "all": return "CTRL_A"
-					case "redo": return "CTRL_Y"
+				const ctrl = "CTRL+"
+				let cmd
+				switch (m) {
+					case "ALL": cmd = "A"; break
+					case "COPY": cmd = "C"; break
+					case "PASTE": cmd = "V"; break
+					case "CUT": cmd = "X"; break
+					case "REDO": cmd = "Y"; break
+					case "UNDO": cmd = "Z"; break
 					default: return _
 				}
+				return [
+					"CTRL",
+					cmd
+				]
+					.join("+")
 			})
 			.replace(
 				new RegExp(`[${[
