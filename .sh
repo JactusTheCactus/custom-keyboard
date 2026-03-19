3d38c115900da33f28389f4bdcbb9acb167e5a33
JactusTheCactus <threateninglyamusing@gmail.com>
2026-03-19 00:25:18 -0700

    Update .sh

--- .sh
+++ .sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 flag() { 
-	for f in "$@"; 
-		do if ! [[ -e ".flags/$f" ]]
-			then return 1
+	for f in "$@"; do
+		if ! [[ -e ".flags/$f" ]]; then
+			return 1
 		fi
 	done
 }
